278c640d3676d422c49a453317f3a06fda576510
Adil Hussain <adilson05uk@gmail.com>
2021-03-05 09:58:38 +0000

    Extract the `XCUIApplication` object from the test methods so it's a class-level property.

--- ShockExperimentUITests/ShockExperimentUITests.swift
+++ ShockExperimentUITests/ShockExperimentUITests.swift
@@ -10,11 +10,15 @@ import Shock
 
 class ShockExperimentUITests: XCTestCase {
     
+    private var application: XCUIApplication!
     private var mockServer: MockServer!
     
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
+        
+        application = XCUIApplication()
+        application.launchArguments = ["-mockServer"]
         
         mockServer = MockServer(port: 6789, bundle: Bundle(for: type(of: self)))
         mockServer.shouldSendNotFoundForMissingRoutes = true
@@ -27,14 +31,15 @@ class ShockExperimentUITests: XCTestCase {
     }
     
     func test_getting_characters_fails_when_route_is_not_setup() {
-        let application = XCUIApplication()
-        application.launchArguments = ["-mockServer"]
+        // When.
         application.launch()
         
+        // Then.
         XCTAssertTrue(application.staticTexts["Failed getting characters."].waitForExistence(timeout: 1))
     }
     
     func test_getting_characters_succeeds_when_simple_route_is_setup() {
+        // Given.
         let route: MockHTTPRoute = .simple(
             method: .get,
             urlPath: "people",
@@ -44,14 +49,15 @@ class ShockExperimentUITests: XCTestCase {
         
         mockServer.setup(route: route)
         
-        let application = XCUIApplication()
-        application.launchArguments = ["-mockServer"]
+        // When.
         application.launch()
         
+        // Then.
         XCTAssertTrue(application.staticTexts["Got 0 characters."].waitForExistence(timeout: 1))
     }
     
     func test_getting_characters_succeeds_when_template_route_is_setup() {
+        // Given.
         let route: MockHTTPRoute = .template(
             method: .get,
             urlPath: "people",
@@ -62,10 +68,10 @@ class ShockExperimentUITests: XCTestCase {
         
         mockServer.setup(route: route)
         
-        let application = XCUIApplication()
-        application.launchArguments = ["-mockServer"]
+        // When.
         application.launch()
         
+        // Then.
         XCTAssertTrue(application.staticTexts["Got 2 characters."].waitForExistence(timeout: 1))
     }
 }
